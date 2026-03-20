76e55db97a504f8f863c83938063b53e09ccf1e8
outercastl3 <outercastl3@proton.me>
2026-03-21 00:09:14 +0100

    AbuseIPDB API logic is done

--- ioc_tool.py
+++ ioc_tool.py
@@ -1,6 +1,7 @@
 import argparse
 import requests
 import json
+import os
 
 def argument_parse():
     pars = argparse.ArgumentParser(
@@ -32,7 +33,28 @@ def domain_api(domain):
     pass
 
 def ip_api(ip_addr):
-    pass
+    api_url = "https://api.abuseipdb.com/api/v2/check"
+    api_key = os.environ.get("ABUSEIPDB_KEY")
+    try:
+        response = requests.get(api_url, headers={"Key": api_key, "Accept": "application/json"}, params={"ipAddress": ip_addr, "maxAgeInDays":90})
+
+         if response.status_code == 200:
+            response_data = response.json()["data"]
+            print(f"IP: {response_data['ipAddress']}")
+            print(f"Abuse Score: {response_data['abuseConfidenceScore']}")
+            print(f"Country: {response_data['countryCode']}")
+            print(f"ISP: {response_data['isp']}")
+            print(f"Total Reports: {response_data['totalReports']}")
+        elif response.status_code == 401:
+            print("Error: Invalid API key")
+            sys.exit(1)
+        else:
+            print(f"Error: {response.status_code}")
+    except requests.exceptions.ConnectionError:
+        print("Error: Connection to AbuseIPDB could not be established")
+    
+    except requests.exceptions.Timeout:
+        print("Error: Requests timed out")
 
 if __name__ == "__main__":
     args = argument_parse()
